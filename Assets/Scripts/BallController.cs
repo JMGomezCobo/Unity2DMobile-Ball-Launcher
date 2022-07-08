8c4cbc605de2d640c3336eddfafe2c21ee15a52d
JMGomezCobo <63131007+JMGomezCobo@users.noreply.github.com>
2022-07-08 09:31:47 -0300

    Player Input Update

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,31 +1,104 @@
+using System.Linq;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
-public class BallController : MonoBehaviour
+namespace BallLauncher
 {
-    [Header("Component Bindings")] 
-    [SerializeField] private Rigidbody2D _ballRigidbody2D;
-    
-    private Camera _mainCamera;
-
-    private void Start()
+    public class BallController : MonoBehaviour
     {
-        _mainCamera = Camera.main;
-    }
+        [Header("Prefab Bindings")]
+        [SerializeField] private GameObject _ballPrefab;
+        
+        [Header("Component Bindings")]
+        [SerializeField] private Rigidbody2D _pivot;
 
-    private void Update()
-    {
-        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        [Header("Settings")] 
+        [SerializeField] private float _respawnDelay = 2f;
+        [SerializeField] private float _detachDelay  = 1f;
+
+        private bool isDragging;
+
+        private Camera _mainCamera;
+        private Rigidbody2D   _currentBallRigidbody;
+        private SpringJoint2D _currentBallSprintJoint;
+
+        #region • Unity methods (4)
+
+        private void Start()
         {
-            _ballRigidbody2D.isKinematic = false;
-            return;
-        }
-        
-        _ballRigidbody2D.isKinematic = true;
-        
-        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-        Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
+            _mainCamera = Camera.main;
 
-        _ballRigidbody2D.position = worldPosition;
+            SpawnBall();
+        }
+
+        private void Update()
+        {
+            if (_currentBallRigidbody == null) { return; }
+
+            if (Touch.activeTouches.Count == 0)
+            {
+                if (isDragging) LaunchBall();
+
+                isDragging = false;
+
+                return;
+            }
+
+            isDragging = true;
+            _currentBallRigidbody.isKinematic = true;
+
+            var worldPosition = HandleInput();
+
+            _currentBallRigidbody.position = worldPosition;
+        }
+
+        private void OnEnable() => EnhancedTouchSupport.Enable();
+
+        private void OnDisable() => EnhancedTouchSupport.Disable();
+
+        #endregion
+
+        #region • Custom methods (4)
+
+        private Vector3 HandleInput()
+        {
+            Vector2 touchPosition = new Vector2();
+
+            touchPosition = Touch.activeTouches.Aggregate(touchPosition, (current, touch) => current + touch.screenPosition);
+            touchPosition /= Touch.activeTouches.Count;
+
+            Vector3 worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
+            
+            return worldPosition;
+        }
+
+        private void SpawnBall()
+        {
+            GameObject ballInstance = Instantiate(_ballPrefab, _pivot.position, Quaternion.identity);
+
+            _currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+            _currentBallSprintJoint = ballInstance.GetComponent<SpringJoint2D>();
+
+            _currentBallSprintJoint.connectedBody = _pivot;
+        }
+
+        private void LaunchBall()
+        {
+            _currentBallRigidbody.isKinematic = false;
+            _currentBallRigidbody = null;
+
+            Invoke(nameof(DetachBall), _detachDelay);
+        }
+
+        private void DetachBall()
+        {
+            _currentBallSprintJoint.enabled = false;
+            _currentBallSprintJoint = null;
+
+            Invoke(nameof(SpawnBall), _respawnDelay);
+        }
+
+        #endregion
     }
 }
